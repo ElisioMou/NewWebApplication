47cee83d78d0735b27569ff66b49127c9c1b950e
ElisioMoura <elisiomou@gmail.com>
2020-10-01 21:20:50 -0300

    App locale, number and date formatting.

--- Models/Seller.cs
+++ Models/Seller.cs
@@ -10,17 +10,19 @@ namespace SalesWebMVC.Models
         public int Id { get; set; }
         [Required(ErrorMessage = "{0} required")]
         [StringLength(60, MinimumLength = 3, ErrorMessage = "{0} size should be between {2} and {1}")]
-        
+             
         public string Name { get; set; }
         [Required(ErrorMessage = "{0} required")]
         [EmailAddress(ErrorMessage = "Enter a valid email")]
         [DataType(DataType.EmailAddress)]
 
+        [DisplayFormat(DataFormatString = "{0:dd/MM/yyyy}")]
+       
         public string Email { get; set; }
         [Required(ErrorMessage = "{0} required")]
         [Display(Name = "Birth Date")]
         [DataType(DataType.Date)]
-        [DisplayFormat(DataFormatString = "{0:dd/MM/yyyy}")]
+        [DisplayFormat(DataFormatString = "{0:MM/dd/yyyy}")]
 
         public DateTime BirthDate { get; set; }
         [Required(ErrorMessage = "{0} required")]
@@ -29,14 +31,16 @@ namespace SalesWebMVC.Models
         [DisplayFormat(DataFormatString = "{0:F2}")]
 
         public double BaseSalary { get; set; }
+       
         public Department Department { get; set; }
         public int DepartmentId { get; set; }
+       
         public ICollection<SalesRecord> Sales { get; set; } = new List<SalesRecord>();
 
         public Seller()
         {
-        }
 
+        }
         public Seller(int id, string name, string email, DateTime birthDate, double baseSalary, Department department)
         {
             Id = id;
